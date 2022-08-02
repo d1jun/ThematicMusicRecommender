efb9b4d70ca92a7babd9e57bfb66402aed106ac3
David Jun <davj534@gmail.com>
2022-08-02 16:22:46 -0700

    fix song selection bug; prevent thread-swtiching

--- src/main.py
+++ src/main.py
@@ -4,8 +4,23 @@ import lyricScraper
 import rpy2.robjects as robjects
 import pandas as pd
 import random
+from rpy2.rinterface_lib import openrlib
+import rpy2.rinterface as rinterface
+# initialize embedded R
+rinterface.initr()
 
 NUM_PLAYLIST_SONGS = 20
+
+# open a temporary csv file to write to; allow transfer of data to topicModel.R
+def tempCSV(lyrics, mode):
+    out_file_temp = open('songTemp.csv', mode, encoding='UTF8')
+    writer_temp = csv.writer(out_file_temp, lineterminator='\n')
+    # on file creation
+    if mode == 'w':
+        writer_temp.writerow(['lyrics'])
+    writer_temp.writerow([lyrics])
+    # close the file to save the data and allow topicModel.R to use it
+    out_file_temp.close()
 
 # reads user's songs from songs.csv, assigns the collection of songs a theme, outputs thematically similar songs in songList.csv
 def matchTheme():
@@ -16,13 +31,30 @@ def matchTheme():
     # Create reader object by passing the file object to reader method
     reader_obj = csv.reader(in_file)
     
-    # execute topic modeling on lyrics from user inputed songs
-    robjects.r.source("topicModel.R", encoding="utf-8")
+    i = 0
+    # store lyrics of songs in songs.csv into songTemp.csv
+    for row in reader_obj:
+        lyrics = lyricScraper.main(row[1], row[2])
+        if lyrics != '':
+            # reset songTemp.csv file and add csv header on first write
+            if i == 0:
+                tempCSV(lyrics, 'w')
+            else:
+                tempCSV(lyrics, 'a')
+            i += 1
+            print("scraped lyrics: " + row[1] + " by " + row[2])
+        else:
+            print("no lyrics: " + row[1] + " by " + row[2])
+
+    # R is not multithreading-friendly; use this to prevent thread switching
+    with openrlib.rlock:
+        # execute topic modeling on lyrics from user inputed songs
+        robjects.r.source("topicModel.R", encoding="utf-8")
 
     # store thematically similar songs to create playlist
     out_file = open('songList.csv', 'w', encoding='UTF8')
     writer = csv.writer(out_file, lineterminator='\n')
-    writer.writerow(['spotify_id','song','artist'])
+    writer.writerow(['spotify_id','song','artist','theme'])
     
     theme = themeSimilarityCalc.main()
     theme_file = pd.read_csv("themes\\" + theme + ".csv")
@@ -30,11 +62,12 @@ def matchTheme():
     songsNum = NUM_PLAYLIST_SONGS if NUM_PLAYLIST_SONGS < rows else rows
     accessedSongsIndex = []
     for i in range(songsNum):
-        randIndex = random.randint(0,songsNum-1)
+        randIndex = random.randint(0,rows-1)
         # reassign until unique index
         while randIndex in accessedSongsIndex:
-            randint = random.randint(0,songsNum-1)
-        writer.writerow([theme_file.iloc[randIndex][0], theme_file.iloc[randIndex][1], theme_file.iloc[randIndex][2]])
+            randIndex = random.randint(0,rows-1)
+        writer.writerow([theme_file.iloc[randIndex][0], theme_file.iloc[randIndex][1], theme_file.iloc[randIndex][2], theme])
+        accessedSongsIndex.append(randIndex)
 
     in_file.close()   
     out_file.close() 
@@ -50,17 +83,6 @@ def createThemeCSV():
         writer = csv.writer(out, lineterminator='\n')
         writer.writerow(["spotify_id","song","artist"])
         out.close()
-
-# open a temporary csv file to write to; allow transfer of data to topicModel.R
-def tempCSV(lyrics, mode):
-    out_file_temp = open('songTemp.csv', mode, encoding='UTF8')
-    writer_temp = csv.writer(out_file_temp, lineterminator='\n')
-    # on file creation
-    if mode == 'w':
-        writer_temp.writerow(['lyrics'])
-    writer_temp.writerow([lyrics])
-    # close the file to save the data and allow topicModel.R to use it
-    out_file_temp.close()
 
 # assign a theme to all the songs in the db
 def processDB():
@@ -92,6 +114,7 @@ def processDB():
             try:
                 robjects.r.source("topicModel.R", encoding="utf-8")
             except:
+                # if topicModel.R errors, assume it's a love song
                 temp = open('tempTheme.txt', 'w', encoding='UTF8')
                 temp_writer = csv.writer(temp, lineterminator='\n')
                 temp_writer.writerow(['love'])
@@ -113,6 +136,7 @@ def processDB():
     in_file.close()   
     out_file.close() 
 
-processDB()
+# functions that should be called
+# processDB()
 # matchTheme()
 # createThemeCSV()
